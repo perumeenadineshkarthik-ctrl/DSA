4a0a5aa62790b0744eca50a3e9c00f5bc7d1b736
perumeenadineshkarthik-ctrl <perumeenadineshkarthik@gmail.com>
2026-08-11 19:43:36 +0530

    first commit

---
+++ fibnoci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibnoci {
+static int fib(int n) {
+//Write your code here
+if(n <= 1){
+return 0;
+
+}else if(n == 2){
+return 1;
+
+}else{
+return fib(n-1)+fib(n-2);
+}
+}
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+int n = scanner.nextInt();
+System.out.println(fib(n));
+scanner.close();
+
+}
+}
+
